67be3c1e735af3be928d3ecd4726b26f05218b83
Ben <b3holmes@uwaterloo.ca>
2023-07-07 21:06:54 -0400

    Updated response curve draw regions

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -50,11 +50,16 @@ struct ResponseCurveComponent : juce::Component, juce::AudioProcessorParameter::
     void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {};
     void timerCallback() override;
     void paint(juce::Graphics& g) override;
-    void updateChain();
+    void resized() override;
 private:
     SimpleEqAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
     MonoChain monoChain;
+    void updateChain();
+    juce::Image background;
+
+    juce::Rectangle<int> getRenderArea();
+    juce::Rectangle<int> getDrawArea();
 };
 
 //==============================================================================
